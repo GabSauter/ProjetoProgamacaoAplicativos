2ebd3ebe15a65b23de1f90ef9e2b40f6d7edaf77
GabrielSauter <gabriel.andrade.sauter@gmail.com>
2023-06-13 17:28:36 -0300

    Fiz a lógica de interface no CategoriaWindow

--- ProjetoProgramacaoAplicativos/src/gui/CategoriaWindow.java
+++ ProjetoProgramacaoAplicativos/src/gui/CategoriaWindow.java
@@ -5,13 +5,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.FlowLayout;
-import javax.swing.JTable;
-import javax.swing.JScrollPane;
+
+import entities.Categoria;
+import service.CategoriaService;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.awt.event.ActionEvent;
 
 public class CategoriaWindow extends JFrame {
 
@@ -22,12 +29,11 @@ public class CategoriaWindow extends JFrame {
 	private JButton btnCadastrar;
 	private JLabel lblNome;
 	private JLabel lblCategoriasCadastradas;
-	private JComboBox comboBox;
+	private JComboBox<String> comboBox;
 	private JLabel lblCadastrarOuEditar;
+	
+	private List<Categoria> categorias;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -40,12 +46,10 @@ public class CategoriaWindow extends JFrame {
 			}
 		});
 	}
-
-	/**
-	 * Create the frame.
-	 */
+	
 	public CategoriaWindow() {
 		this.initComponents();
+		this.carregarComboBox();
 	}
 	
 	public void initComponents() {
@@ -61,7 +65,7 @@ public class CategoriaWindow extends JFrame {
 		lblCadastrarOuEditar.setBounds(12, 20, 255, 15);
 		contentPane.add(lblCadastrarOuEditar);
 		
-		comboBox = new JComboBox();
+		comboBox = new JComboBox<String>();
 		comboBox.setBounds(195, 47, 209, 19);
 		contentPane.add(comboBox);
 		
@@ -79,15 +83,96 @@ public class CategoriaWindow extends JFrame {
 		contentPane.add(lblNome);
 		
 		btnCadastrar = new JButton("Cadastrar");
+		btnCadastrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnCadastrarAction();
+			}
+		});
 		btnCadastrar.setBounds(12, 135, 127, 25);
 		contentPane.add(btnCadastrar);
 		
 		btnEditar = new JButton("Editar");
+		btnEditar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnEditarAction();
+			}
+		});
 		btnEditar.setBounds(151, 135, 124, 25);
 		contentPane.add(btnEditar);
 		
 		btnExcluir = new JButton("Excluir");
+		btnExcluir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnExcluirAction();
+			}
+		});
 		btnExcluir.setBounds(287, 135, 117, 25);
 		contentPane.add(btnExcluir);
+	}
+
+	private void btnExcluirAction() {
+		Categoria categoria = new Categoria();
+		categoria.setNome((String) comboBox.getSelectedItem());
+		
+		int resposta = JOptionPane.showConfirmDialog(this, "Deseja realmente excluir a categoria: " + categoria.getNome() + "?", "Confirmação", JOptionPane.YES_NO_OPTION);
+		
+		if (resposta == JOptionPane.YES_OPTION) {
+			try {
+		        new CategoriaService().excluir(categoria);
+		        JOptionPane.showMessageDialog(this, "Categoria excluida", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+		        carregarComboBox();
+		    } catch (SQLException | IOException e) {
+		        e.printStackTrace();
+		        JOptionPane.showMessageDialog(this, "Erro ao excluir categoria.", "Erro", JOptionPane.ERROR_MESSAGE);
+		    }
+		}
+	}
+
+	private void btnEditarAction() {
+		Categoria categoria = new Categoria();
+		categoria.setNome((String) comboBox.getSelectedItem());
+		String novoNomeCategoria = textField.getText();
+		
+		int resposta = JOptionPane.showConfirmDialog(this, "Deseja realmente editar o nome da categoria: " + categoria.getNome() + " para " + novoNomeCategoria + "?", "Confirmação", JOptionPane.YES_NO_OPTION);
+
+		if (resposta == JOptionPane.YES_OPTION) {
+			try {
+		        new CategoriaService().atualizar(novoNomeCategoria, categoria);
+		        JOptionPane.showMessageDialog(this, "Categoria editada com sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+		        carregarComboBox();
+		    } catch (SQLException | IOException e) {
+		        e.printStackTrace();
+		        JOptionPane.showMessageDialog(this, "Erro ao editar categoria.", "Erro", JOptionPane.ERROR_MESSAGE);
+		    }
+		}
+	}
+
+	private void btnCadastrarAction() {
+		Categoria categoria = new Categoria();
+		categoria.setNome(textField.getText());
+		
+		try {
+	        new CategoriaService().cadastrar(categoria);
+	        JOptionPane.showMessageDialog(this, "Categoria cadastrada com sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+	        carregarComboBox();
+	    } catch (SQLException | IOException e) {
+	        e.printStackTrace();
+	        JOptionPane.showMessageDialog(this, "Erro ao cadastrar categoria.", "Erro", JOptionPane.ERROR_MESSAGE);
+	    }
+	}
+	
+	private void carregarComboBox() {
+		
+		try {
+			
+			this.categorias = new CategoriaService().buscarTodos();
+			
+			this.comboBox.removeAllItems();
+			for(Categoria categoria : categorias) {
+				this.comboBox.addItem(categoria.getNome());
+			}
+		} catch (SQLException | IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- ProjetoProgramacaoAplicativos/src/gui/RendimentoWindow.java
+++ ProjetoProgramacaoAplicativos/src/gui/RendimentoWindow.java
@@ -1,5 +1,5 @@
-
 package gui;
+
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -7,21 +7,20 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
-import javax.swing.JFormattedTextField;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.text.MaskFormatter;
-import javax.swing.text.NumberFormatter;
-import javax.swing.JSeparator;
+
+import entities.Categoria;
+import service.CategoriaService;
+
 import javax.swing.border.TitledBorder;
 import java.awt.Font;
-import java.text.NumberFormat;
-import java.text.ParseException;
-
 import javax.swing.JRadioButton;
-import javax.swing.SwingConstants;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 import java.awt.event.ActionEvent;
 
 public class  RendimentoWindow{
@@ -45,22 +44,23 @@ public class  RendimentoWindow{
     private JPanel panelTipoRendimento;
     private JRadioButton rdbtnOcasional;
     private JRadioButton rdbtnMensal;
+    
+    private List<Categoria> categorias;
 
+	public static void main(String[] args) { EventQueue.invokeLater(() -> { try {
+		RendimentoWindow window = new RendimentoWindow();
+		window.frame.setVisible(true); } catch (Exception e) { e.printStackTrace(); }
+	}); }
+	
 	/**
-	 * Launch the application.
+	 * Create the frame.
+	 * @wbp.parser.entryPoint
 	 */
-	  public static void main(String[] args) { EventQueue.invokeLater(() -> { try {
-	  RendimentoWindow window = new RendimentoWindow();
-	  window.frame.setVisible(true); } catch (Exception e) { e.printStackTrace(); }
-	  }); }
-	 
-/**
- * Create the frame.
- * @wbp.parser.entryPoint
- */
     public RendimentoWindow() {
         this.initComponents();
+        this.carregarComboBox();
     }
+    
 
     private void initComponents() {
         frame = new JFrame();
@@ -84,10 +84,20 @@ public class  RendimentoWindow{
         panelCadastro.add(txtRendimento);
         
         btnCadastrarRendimento = new JButton("Cadastrar");
+        btnCadastrarRendimento.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		btnCadastrarRendimento();
+        	}
+        });
         btnCadastrarRendimento.setBounds(269, 135, 105, 23);
         panelCadastro.add(btnCadastrarRendimento);
         
         btnLimparCampos = new JButton("Limpar campos");
+        btnLimparCampos.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		btnLimparCamposAction();
+        	}
+        });
         btnLimparCampos.setBounds(386, 135, 150, 23);
         panelCadastro.add(btnLimparCampos);
         
@@ -121,18 +131,18 @@ public class  RendimentoWindow{
         
         panelTipoRendimento = new JPanel();
         
-                panelTipoRendimento.setBorder(new TitledBorder(null, "Tipo de rendimento", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-                panelTipoRendimento.setBounds(12, 108, 247, 50);
-                panelCadastro.add(panelTipoRendimento);
-                panelTipoRendimento.setLayout(null);
-                
-                rdbtnOcasional = new JRadioButton("Ocasional");
-                rdbtnOcasional.setBounds(8, 19, 95, 23);
-                panelTipoRendimento.add(rdbtnOcasional);
-                
-                rdbtnMensal = new JRadioButton("Mensal");
-                rdbtnMensal.setBounds(136, 19, 89, 23);
-                panelTipoRendimento.add(rdbtnMensal);
+        panelTipoRendimento.setBorder(new TitledBorder(null, "Tipo de rendimento", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+        panelTipoRendimento.setBounds(12, 108, 247, 50);
+        panelCadastro.add(panelTipoRendimento);
+        panelTipoRendimento.setLayout(null);
+        
+        rdbtnOcasional = new JRadioButton("Ocasional");
+        rdbtnOcasional.setBounds(8, 19, 95, 23);
+        panelTipoRendimento.add(rdbtnOcasional);
+        
+        rdbtnMensal = new JRadioButton("Mensal");
+        rdbtnMensal.setBounds(136, 19, 89, 23);
+        panelTipoRendimento.add(rdbtnMensal);
 
         panelRendimento = new JPanel();
         panelRendimento.setBorder(new TitledBorder(null, "Rendimentos cadastrados", TitledBorder.LEADING, TitledBorder.TOP, null, null));
@@ -141,27 +151,68 @@ public class  RendimentoWindow{
         panelRendimento.setLayout(null);
 
         btnEditarRendimento = new JButton("Editar");
+        btnEditarRendimento.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		btnEditarRendimentoAction();
+        	}
+        });
         btnEditarRendimento.setBounds(333, 162, 100, 23);
         panelRendimento.add(btnEditarRendimento);
 
         btnExcluirRendimento = new JButton("Excluir");
+        btnExcluirRendimento.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		btnExcluirRendimentoAction();
+        	}
+        });
         btnExcluirRendimento.setBounds(438, 162, 100, 23);
         panelRendimento.add(btnExcluirRendimento);
         
-                scrollPane = new JScrollPane();
-                scrollPane.setBounds(12, 14, 526, 136);
-                panelRendimento.add(scrollPane);
+        scrollPane = new JScrollPane();
+        scrollPane.setBounds(12, 14, 526, 136);
+        panelRendimento.add(scrollPane);
                 
-                        table = new JTable();
-                        table.setModel(new DefaultTableModel(
-                            new Object[][] {
-                            },
-                            new String[] {
-                                "Categoria", "Rendimento","Mensal", "Ocasional", "Total Anual"
-                            }
-                        ));
-                        scrollPane.setViewportView(table);
+        table = new JTable();
+        table.setModel(new DefaultTableModel(
+            new Object[][] {
+            },
+            new String[] {
+                "Categoria", "Rendimento","Mensal", "Ocasional", "Total Anual"
+            }
+        ));
+        scrollPane.setViewportView(table);
     }
+    
+    private void carregarComboBox() {
+		try {
+			
+			this.categorias = new CategoriaService().buscarTodos();
+			
+			this.cbCategoria.removeAllItems();
+			for(Categoria categoria : categorias) {
+				this.cbCategoria.addItem(categoria.getNome());
+			}
+			
+		} catch (SQLException | IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    private void btnExcluirRendimentoAction() {
+		
+	}
+
+	private void btnEditarRendimentoAction() {
+		
+	}
+
+	private void btnLimparCamposAction() {
+		
+	}
+
+	private void btnCadastrarRendimento() {
+		
+	}
 
 	private void btnAddCategoriaAction() {
 			new CategoriaWindow().setVisible(true);
